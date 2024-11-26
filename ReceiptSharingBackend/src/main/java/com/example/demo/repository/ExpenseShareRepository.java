faad67a367752f9b52bb8d268cedf6be9297c4cb
dariasenyaninova <162359183+dariasenyaninova@users.noreply.github.com>
2024-11-26 20:46:30 +0200

    Delete ReceiptSharingBackend directory

--- ReceiptSharingBackend/src/main/java/com/example/demo/repository/ExpenseShareRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.ExpenseShare;
-import com.example.demo.entity.ExpenseShareId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExpenseShareRepository extends JpaRepository<ExpenseShare, ExpenseShareId> {
-}
